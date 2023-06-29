f4749303f00479dec0a5404a6025b0b6cc1d411c
Bishnu Lamsal <13574617+bishnulamsal@users.noreply.github.com>
2023-06-29 12:53:05 +0545

    Update bnep-date.js

--- bnep-date.js
+++ bnep-date.js
@@ -1,4 +1,4 @@
-<script>
+
     const nepali_years_and_days_in_months = [
     [2000, 30, 32, 31, 32, 31, 30, 30, 30, 29, 30, 29, 31],
     [2001, 31, 31, 32, 31, 31, 31, 30, 29, 30, 29, 30, 30],
@@ -470,4 +470,4 @@ function englishToNepaliNumber(number) {
 }
   
   
-  </script>
+
